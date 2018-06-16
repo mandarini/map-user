55b2ebe76864f1b9a55d2f7fa2e29e9ff7f0dd30
Katerina <sk.katherine@gmail.com>
2018-06-16 11:38:49 +0300

    scaffolings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ScriptLoadService } from './script-load.service';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ScriptLoadService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/script-load.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ScriptLoadService {
+
+  constructor() {}
+
+  public loadScript(url, id, c): void {
+    if (!document.getElementById(id)) {
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = url;
+      script.id = id;
+      script.addEventListener('load', function (e) {
+        c(null, e);
+      }, false);
+      document.head.appendChild(script);
+    }
+  }
+}
