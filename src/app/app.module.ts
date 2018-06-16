128fbf7acbcc7692e40414ebd58b9e0817b6212c
Katerina <sk.katherine@gmail.com>
2018-06-16 11:53:10 +0300

    added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ScriptLoadService } from './script-load.service';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+
+const config = {
+  apiKey: "AIzaSyC3X9Wdp9jgpOR2nCVhe4a8QV3nsns9Ie8",
+  authDomain: "map-psybercity.firebaseapp.com",
+  databaseURL: "https://map-psybercity.firebaseio.com",
+  projectId: "map-psybercity",
+  storageBucket: "",
+  messagingSenderId: "114738124484"
+};
+
 
 @NgModule({
   declarations: [
@@ -11,7 +24,9 @@ import { MapComponent } from './map/map.component';
     MapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(config),
+    AngularFireDatabaseModule
   ],
   providers: [ScriptLoadService],
   bootstrap: [AppComponent]
