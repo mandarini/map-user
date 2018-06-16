2d22c3449345301a284f37f2e198735e88eab5af
Katerina <sk.katherine@gmail.com>
2018-06-16 16:15:09 +0300

    write and get data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,29 +4,33 @@ import { ScriptLoadService } from './script-load.service';
 
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { ListComponent } from './list/list.component';
 
 const config = {
-  apiKey: "AIzaSyC3X9Wdp9jgpOR2nCVhe4a8QV3nsns9Ie8",
-  authDomain: "map-psybercity.firebaseapp.com",
-  databaseURL: "https://map-psybercity.firebaseio.com",
-  projectId: "map-psybercity",
-  storageBucket: "",
-  messagingSenderId: "114738124484"
+    apiKey: "AIzaSyC3X9Wdp9jgpOR2nCVhe4a8QV3nsns9Ie8",
+    authDomain: "map-psybercity.firebaseapp.com",
+    databaseURL: "https://map-psybercity.firebaseio.com",
+    projectId: "map-psybercity",
+    storageBucket: "map-psybercity.appspot.com",
+    messagingSenderId: "114738124484"
 };
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(config),
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
+    AngularFirestoreModule
   ],
   providers: [ScriptLoadService],
   bootstrap: [AppComponent]
---
+++ src/app/list/list.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { FirebaseApp } from 'angularfire2';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+  
+  itemsRef: AngularFireList<any>;
+  items: Observable<any[]>;
+  otheritems: Observable<any[]>;
+
+  constructor(db: AngularFireDatabase) {
+    this.itemsRef = db.list('/messages');
+    // Use snapshotChanges().map() to store the key
+    this.items = this.itemsRef.snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+      )
+    );
+
+    // this.artists = db.list('/messages').valueChanges();
+
+    // this.artists.subscribe(x => {
+    //   console.log(x);
+    // });
+
+  }
+  addItem(newName: string) {
+    this.itemsRef.push({ text: newName });
+  }
+  updateItem(key: string, newText: string) {
+    this.itemsRef.update(key, { text: newText });
+  }
+  deleteItem(key: string) {
+    this.itemsRef.remove(key);
+  }
+  deleteEverything() {
+    this.itemsRef.remove();
+  }
+
+}
